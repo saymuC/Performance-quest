ad4fe6b15e1bec78a5fd696b80ce46ebd68ae1c8
SaymuC <saymonhenrique2374@gmail.com>
2026-08-25 10:37:46 -0400

    Feat: validação do year (ano)

--- backend/src/routes/questions.js
+++ backend/src/routes/questions.js
@@ -6,8 +6,30 @@ const router = express.Router();
 
 router.get("/", async (req, res) => {
     try {
-        const year = req.query.year || 2023;
+        const { year } = req.query;
 
+        // validação 1: o valor year existe?
+        if (year === undefined || year === "") {
+            return res.status(400).json({
+                error: "O parâmetro Ano é obrigatório."
+            });
+        }
+
+        const yearNumber = Number(year);
+
+        //validação 2: o valor year é um numero?
+        if (!Number.isInteger(yearNumber)) {
+            return res.status(422).json({
+                error: "O ano deve ser um número inteiro."
+            });
+        }
+
+        //validação 3: o valor year está entre 2009 e 2023?
+        if (yearNumber < 2009 || yearNumber > 2023) {
+            return res.status(422).json({
+                error: "O ano de busca deve estar entre 2009 e 2023."
+            });
+        }
         const data = await getQuestions(year);
 
         const questions = data.questions.map(sanitizeQuestion)
